dba98528a781ea83f932d2abacf94cbf30e4b3b4
Dhia Abbassi <dhia.absi@gmail.com>
2016-05-17 19:44:26 +0200

    Updated config

--- config/settings/base.py
+++ config/settings/base.py
@@ -40,7 +40,7 @@ SECRET_KEY = get_env_variable('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
